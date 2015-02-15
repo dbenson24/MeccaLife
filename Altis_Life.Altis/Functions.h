4a40ffd9412977ddd1345ffe305e98396b7ff262
KingMattimus <Iunnamed@gmail.com>
2015-02-14 20:34:05 -0600

    Economy first shot

--- Altis_Life.Altis/Functions.h
+++ Altis_Life.Altis/Functions.h
@@ -381,7 +381,14 @@ class Life_Client_Core
 		class sendChannel {};		
 		
 	};
-	
+	class economy
+	{
+		file = "core\economy";
+		class virt_updatePrice {};
+		class addsubstract {};
+		class openEconomy {};
+		class virt_updateEconomy {};
+	};
 	class Cam
 	{
 	   file ="core";
--- life_server/config.cpp
+++ life_server/config.cpp
@@ -139,6 +139,8 @@ class CfgFunctions
 			class cleanupRequest {};
 			class setObjVar {};
 			class keyManagement {};
+			class getPrices {};
+            class ajustprices {};
 		};
 		
 		class Housing
