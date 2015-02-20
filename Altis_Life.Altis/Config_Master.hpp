e078eed85f944d87decf1e9821f6c6711f1d2a13
KingMattimus <Iunnamed@gmail.com>
2015-02-20 11:40:51 -0600

    frew last changes checking in dev now

--- Altis_Life.Altis/Config_Master.hpp
+++ Altis_Life.Altis/Config_Master.hpp
@@ -130,7 +130,7 @@ class VirtualItems {
 	VITEMMACRO(cocaine_processed, "STR_Item_CocaineP", "cocaineProcessed", 2, -1, 3000, true, -1, "")
 	VITEMMACRO(froglsd, "STR_Item_Froglsd", "froglsd", 2, -1, 2500, true, -1, "")
 	VITEMMACRO(frog, "STR_Item_Frog", "frog", 2, -1, 2500, true, -1, "")
-	VITEMMACRO(Magicm, "STR_Item_Magicm", "magicm", 5, -1, 1950, false, -1, "")
+	VITEMMACRO(Magicm, "STR_Item_Magicm", "magicm", 1, -1, 1200, false, -1, "")
 
 	//DrinkC
 	VITEMMACRO(redgull, "STR_Item_RedGull", "redgull", 1, 1500, 200, false, 100, "icons\ico_redgull.paa")
