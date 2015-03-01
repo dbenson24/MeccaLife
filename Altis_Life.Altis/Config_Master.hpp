c41089d58c4f183f9b522aa9f7fe8b424169f61a
KingMattimus <Iunnamed@gmail.com>
2015-03-01 16:25:04 -0600

    Go Go Cal

--- Altis_Life.Altis/Config_Master.hpp
+++ Altis_Life.Altis/Config_Master.hpp
@@ -145,6 +145,8 @@ class VirtualItems {
 	VITEMMACRO(redgull, "STR_Item_RedGull", "redgull", 1, 1500, 200, false, 100, "icons\ico_redgull.paa")
 	VITEMMACRO(coffee, "STR_Item_Coffee", "coffee", 1, 10, 5, false, 100, "icons\coffee.paa")
 	VITEMMACRO(waterBottle, "STR_Item_WaterBottle", "waterBottle", 1, 10, 5, false, 100, "icons\ico_waterBottle.paa")
+	VITEMMACRO(largesoda, "STR_Item_largesoda", "largesoda", 1, 10, 5, false, 100, "icons\lsoda.paa")
+
 
 	//Food
 	VITEMMACRO(apple, "STR_Item_Apple", "apple", 1, 65, 50, false, 10, "icons\food.paa")
