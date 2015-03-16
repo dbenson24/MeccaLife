ae0eff6770b3b8b6436f66236eca3c27d7be329f
Elemtael <addison.bethea@gmail.com>
2015-03-16 17:24:38 -0600

    Final Updates to Core

--- Configs & Bats/Startup.bat
+++ Configs & Bats/Startup.bat
@@ -1,19 +1,19 @@
 @echo off
 
+timeout 5
 REM taskkill /f /fi "status eq not responding" /im arma3server.exe
 taskkill /f /im arma3server.exe
 taskkill /im Bec.exe
 
-timeout 9
+timeout 5
 cd E:\arma3server\
-start arma3server.exe "-config=E:\_arma3serverData\serverConfig.cfg" "-cfg=E:\_arma3serverData\server_network.cfg" "-BEPath=E:\_arma3serverData\battleye" "-profiles=E:\_arma3serverData\profiles" "-mod=@life_server;@extDB" "-enableHT"
+start arma3server.exe "-config=E:\_arma3serverData\Configs & Bats\serverConfig.cfg" "-cfg=E:\_arma3serverData\Configs & Bats\server_network.cfg" "-BEPath=E:\_arma3serverData\battleye" "-profiles=E:\_arma3serverData\Profiles" "-mod=@life_server;@extDB" "-enableHT"
 echo ARMA 3 Server has started.
 
 timeout 10
 set becpath="E:\_arma3serverData\Bec"
 cd /d %becpath%
 start "" /min "Bec.exe" -f Config.cfg
-timeout 3
 echo Battleye has started.. 
 echo Exiting.
 timeout 3
