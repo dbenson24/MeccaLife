38fb855b8fbf741149b7efe892aee2d3ba382c74
dbenson24 <derek.benson@tufts.edu>
2015-04-03 18:36:29 +0000

    Another broken Ifrit

--- Altis_Life.Altis/Config_Vehicles.hpp
+++ Altis_Life.Altis/Config_Vehicles.hpp
@@ -426,7 +426,7 @@ class CfgVehicles {
 
 	class O_MRAP_02_F {
         vItemSpace = 150;
-        price = pifrit;
+//        price = pifrit;
         storageFee[] = { 450, 0, 0, 0 };
 		garageSell[] = { 65000, 0, 0, 0 };
         insurance = 2500;
@@ -436,7 +436,7 @@ class CfgVehicles {
 
 	class I_MRAP_03_F {
         vItemSpace = 58;
-//        price = pifrit;
+        price = pifrit;
         storageFee[] = { 1000, 0, 0, 0 };
         insurance = 2500;
         chopShop = 5000;
