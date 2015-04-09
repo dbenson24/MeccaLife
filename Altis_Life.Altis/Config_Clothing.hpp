aacf1942a744c4cf6dde80e54b59757d49cf4d8e
dbenson24 <derek.benson@tufts.edu>
2015-04-09 04:02:54 +0000

    fix for vests

--- Altis_Life.Altis/Config_Clothing.hpp
+++ Altis_Life.Altis/Config_Clothing.hpp
@@ -321,11 +321,11 @@ class Clothing {
 			{ "V_Rangemaster_belt", "", 1800, { "", "", -1 } },
 			{ "V_PlateCarrier2_rgr", "", 50000, { "", "", -1 } },
 			{ "V_HarnessOGL_gry", "SuicideVest", 500000, { "", "", -1 } },
-			{ "V_PlateCarrierSpec_mtp", "Special Carrier", 500000, { "", "", -1 } },
-			{ "V_PlateCarrierSpec_rgr", "Special Carrier", 500000, { "", "", -1 } },
-			{ "V_PlateCarrierGL_rgr", " Carrier GL Green", 1000000, { "", "", -1 } }, // broken
-			{ "V_PlateCarrierIAGL_dgtl", "Carrier Rig Donor", 25000, { "life_donator", "SCALAR", 2 } }, 
-			{ "V_PlateCarrierIGL_mtp", "Carrier GL Rig DonorSkin", 1000000, { "life_donator", "SCALAR", 2 } } // broken
+			{ "V_PlateCarrierSpec_mtp", "Special Carrier", 250000, { "", "", -1 } },
+			{ "V_PlateCarrierSpec_rgr", "Special Carrier", 250000, { "", "", -1 } },
+			{ "V_PlateCarrierGL_rgr", "Carrier GL Green", 500000, { "", "", -1 } },
+			{ "V_PlateCarrierIAGL_dgtl", "Carrier Rig Donor", 350000, { "life_donator", "SCALAR", 2 } }, 
+			{ "V_PlateCarrierIAGL_oli", "Carrier GL Rig DonorSkin", 350000, { "life_donator", "SCALAR", 2 } }
 			
 		};
 
